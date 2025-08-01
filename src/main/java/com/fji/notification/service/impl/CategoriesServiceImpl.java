df12c96fe6cd3790086e7374a654d8301c07e6d3
Ivan F <lunabotics.unam.ivanflo@gmail.com>
2025-07-31 22:25:50 -0600

    Cleanup unneeded classes.

--- src/main/java/com/fji/notification/service/impl/CategoriesServiceImpl.java
+++ src/main/java/com/fji/notification/service/impl/CategoriesServiceImpl.java
@@ -1,23 +1,19 @@
 package com.fji.notification.service.impl;
 
-import com.fji.notification.model.Category;
-import com.fji.notification.repository.CategoriesRepository;
+import com.fji.notification.model.CategoryEnum;
 import com.fji.notification.service.CategoriesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CategoriesServiceImpl implements CategoriesService {
 
-    private final CategoriesRepository categoriesRepository;
-
-
     @Override
     public List<String> getAllCategoryNames() {
-        return categoriesRepository.getAllCategories().stream().map(Category::getName).sorted().collect(Collectors.toList());
+        return Arrays.stream(CategoryEnum.values()).map(CategoryEnum::getName).toList();
     }
 }
